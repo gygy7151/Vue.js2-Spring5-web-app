757a8f3d7e57a051299d9ce4915cfcbe3c4584e0
Mac <gygy7151@gmail.com>
2021-04-07 02:12:25 +0900

    210407_Update p.20js
    [함수호출방법 4가지]
    1. 생성자 함수 호출 : new User()
    2. 직접함수 호출: ask()
    3. 메소드 호출: user, speak()
    4. 컨텍스트 변경호출: ask, call(this) 또는 ask.apply(this)
    
    생성자 함수호출시 함수 본문내의 this가 다른 3가지 유형의 함수 호출로 만들어진 인스턴스를 제외하고 생성자로 생선된 객체를 참조한다.
    
    직접함수 호출의 경우 함수본문 내부의 this가 다른 3가지 유형 함수호출로 만들어진 인스턴스 제외하고 '전역컨텍스트'를 참조한다.
    
    메소드 호출방법은 제외 조건은 위와 상동하되 메소드에 속한 객체를 참조한다.
    
    컨텍스트 호출 변경 방법도 조건은 위와 상동하나, call() 메소드의 첫번째 인자로 전달 받은 객체를 참조한다.

--- chap01/p.20.js
+++ chap01/p.20.js
@@ -16,8 +16,27 @@ function User (name) { //User생성함수는 전역스코프(global Scope)에서
             this.constructor.name + '"    context.');
         console.log('Who am I?"' + this.name + '"');
     }
-    ask();
+    ask(); 
+    // Asking from "Window" context.
+    // Who am I? "Unknown"
+    ask.call(this); //call() 메소드는 Function객체에 의해 생성된 메소드이자 객체임 ex> call(), apply(), bind가 있음
+    // Asking from "User" context.
+    // Who am I? "Ted"
+
+    ask.bind(this)();
+    // Asking from "User" context.
+    // Who am I? "Ted"
+    //bind는 새로운 함수의 실행 컨텍스트로 전달받은 첫번째 인자와 바인딩될 새로운 함수를 생성한다. 
+    //또한 즉시 실행이외에도 새로운 함수 또는 변소를 객체의 메소드로 할당할 수 있다.
 }
 var name = 'Unknown'; //여기부터 전역 코드 : 자바스크립트 프로그램이 시작되는 곳부터 수행되는 코드. 브라우저의 경우 Window 객체가 존재한다.
 var user = new User('Ted');
 user.speak();
+// Ted is speaking from "User" context.
+// Drinking in "Window" context.
+
+user.speak.apply({name: 'Jack'})
+// Jack is speaking from "Object" context.
+// Drinking in "Window" context.
+//실행 컨텍스트를 변경하는 방법으로 call()과 apply()메소드를 고려할 수 있다.
+ 
